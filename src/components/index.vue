888e78a898a2550f97fcfa0fdbfcf3a61f21c931
Eden_one <1158810459@qq.com>
2018-05-31 21:47:06 +0800

    updata nowShowing

--- src/components/index.vue
+++ src/components/index.vue
@@ -32,10 +32,20 @@
     <content>
       <router-view></router-view>
     </content>
-    <footer></footer>
+    <footer>
+      <div class="footer-content">
+        <div class="footer-top-info">
+          <div class="left-logo">
+            <img src="../../static/img/logoFont.png">
+            <p>由烫烫烫提供技术支持</p>
+          </div>
+        </div>
+        <div class="footer-bottom-info">
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
-
 <script>
   export default {
     name: 'HelloWorld',
@@ -154,5 +164,36 @@
   content{
     width: 100%;
     height:auto;
+  }
+  footer{
+    width: 100%;
+    height:12rem;
+    background-color: #2b2b2b;
+    border-top: 2px solid red;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .footer-content{
+    width:1100px;
+    height:12rem;
+  }
+  .footer-top-info{
+    width: 100%;
+    height:8rem;
+    border:1px solid;
+  }
+  .footer-top-info .left-logo{
+    height:100%;
+    width: 15rem;
+    color:#cccccc;
+    text-align: center;
+    letter-spacing: 0.2rem;
+    font-size: 0.8rem;
+  }
+  .left-logo img{
+    width: 70%;
+    height:auto;
+    margin: 0 auto;
   }
 </style>
--- src/components/nowShowing.vue
+++ src/components/nowShowing.vue
@@ -1,11 +1,19 @@
 <template>
 <div class="movieListBox">
   <ul class="movieList">
-    <li v-for="item in afterShowingList">
+    <li v-for="item in afterShowingList" @mouseenter="showMovieInfo" @mouseleave="closeMovieInfo">
       <div class="movieImg">
         <img :src="'https://img.alicdn.com/bao/uploaded/'+item.poster" />
       </div>
-      <div class="movieInfo"></div>
+      <div class="movieInfo">
+        <p>导演:{{item.director}}</p>
+        <p>主演:{{item.leadingRole}}</p>
+        <p>类型:{{item.type}}</p>
+        <p>地区:{{item.country}}</p>
+        <p>语言:{{item.language}}</p>
+        <p>片长:{{item.duration}}</p>
+      </div>
+      <p class="movie-name"><span>{{item.showName}}</span></p>
       <p class="openTime"><span>上映时间:{{item.openTime}}</span></p>
     </li>
   </ul>
@@ -18,10 +26,11 @@ export default {
   data(){
     return {
       movieList:"",
-      afterShowingList:""
+      afterShowingList:"",
+      showInfo:false
     }
   },
-  beforeMount(){
+  mounted(){
     this.getMovieInfo();
   },
   methods:{
@@ -50,6 +59,16 @@ export default {
 //          alert("请求出错！");
 //        }
 //      })
+    },
+    showMovieInfo:function(e){
+      let movieDom = e.currentTarget.getElementsByClassName('movieInfo')[0];
+      movieDom.style.visibility="visible";
+      movieDom.classList.add("fadeIn");
+    },
+    closeMovieInfo:function(e){
+      let movieDom = e.currentTarget.getElementsByClassName('movieInfo')[0];
+      movieDom.style.visibility="hidden";
+      movieDom.classList.add("fadeOut");
     }
   }
 }
@@ -78,24 +97,56 @@ export default {
     cursor: pointer;
   }
   .movieImg{
+    position: absolute;
+    z-index: 1;
     width:161px;
     height:224px;
-    z-index: -1;
   }
-  /*.movieInfo{*/
-    /*width:161px;*/
-    /*height:224px;*/
-    /*z-index: 1;*/
-  /*}*/
+  .movieInfo {
+    position: absolute;
+    visibility: hidden;
+    width: 141px;
+    height: 214px;
+    z-index: 11;
+    padding-top:10px;
+    padding-left: 20px;
+    font-size: 0.8rem;
+    color: whitesmoke;
+    text-align: start;
+    background-color: rgba(0,0,0,0.57);
+  }
+  .movieInfo p{
+    width:130px;
+    height:1.5rem;
+    overflow: hidden;
+    line-height: 1.5rem;
+    margin-top: 0.25rem;
+    letter-spacing: 2px;
+  }
   .movieImg img{
     width: 100%;
     height:100%;
   }
+  .movie-name{
+    position: absolute;
+    display: block;
+    bottom: 14px;
+    width:141px;
+    padding: 0 10px;
+    height:1.5rem;
+    line-height: 1.5rem;
+    text-align: start;
+    overflow: hidden;
+    color:white;
+    z-index: 11;
+    background: linear-gradient(rgba(128,128,128,0.6),rgba(100,100,100,0.5), rgba(0,0,0,0.8));
+  }
   .openTime{
+    margin-top:240px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    font-size: 0.8rem;
+    font-size: 0.9rem;
   }
 </style>
